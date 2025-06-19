3153cce8ebfa168c97ab134a951551e3c47142bb
Clarence Coronel <clarence@offshorly.com>
2025-06-19 13:14:38 +0800

    fix: building readme invalid markdown

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,29 +1,16 @@
-import { exec } from 'child_process'
 import path from 'path'
+import { exec } from 'child_process'
 import { promisify } from 'util'
+
+const execAsync = promisify(exec)
 
 function hasAnyArrayWithValues<T>(arrayOfArrays: T[][]) {
   return arrayOfArrays.some((innerArray) => innerArray.length > 0)
 }
+
 function generateVSCodeLink(filePath: string) {
   const absolutePath = path.resolve(filePath)
-  const encodedPath = absolutePath
-  return `"vscode://${encodedPath}"`
-}
-
-const execAsync = promisify(exec)
-
-async function installVsCodeExtension(extensions: string[]) {
-  try {
-    for (const extension of extensions) {
-      const { stdout, stderr } = await execAsync(
-        `code --install-extension ${extension}`,
-      )
-      console.log(`Successfully installed ${extension}:`, stdout)
-    }
-  } catch (error) {
-    console.error('Error during installation:', error)
-  }
+  return `"vscode://${absolutePath}"`
 }
 
 interface GroupedData {
@@ -32,59 +19,53 @@ interface GroupedData {
 
 function groupByCategories(data: string[][]): GroupedData {
   return data.reduce((acc: GroupedData, item: string[]) => {
-    const lastElement = item.pop() as string // Remove and get the last element (the actual item)
+    const lastElement = item.pop() as string
     let currentLevel = acc
 
-    // Traverse through all elements except the last one (which is now removed)
     item.forEach((category) => {
-      // Check if the category already exists, otherwise create a new object
       if (!currentLevel[category]) {
         currentLevel[category] = {}
       }
-      // Move to the next level
       currentLevel = currentLevel[category] as GroupedData
     })
 
-    // Add the actual item to the lowest level
     if (!currentLevel['_items']) {
       currentLevel['_items'] = []
     }
+
     ;(currentLevel['_items'] as string[]).push(lastElement)
 
     return acc
   }, {})
 }
 
-function transformFilename(filename: string): string{
-  return filename.split("(")[0].replace(".md", "").trim();
+function transformFilename(filename: string): string {
+  return filename.split('(')[0].replace('.md', '').trim()
 }
 
 function generateGitHubMarkdownList(
   obj: GroupedData,
   pathPrefix: string = '',
-  isSubCategory: boolean = false
+  isSubCategory: boolean = false,
 ): string {
   let markdown = ''
 
-  Object.keys(obj).forEach((key, i, arr) => {
+  Object.keys(obj).forEach((key) => {
     if (key === '_items') {
-      // If key is '_items', format items as markdown list with GitHub-compatible links
-      markdown += (obj[key] as string[])
+      const items = (obj[key] as string[])
         .map((item) => {
           const itemPath = `${pathPrefix}/${item}`
-          return `\n- [${transformFilename(item)}](${new URL(itemPath).toString()})`
+          return `- [${transformFilename(item)}](${new URL(itemPath).toString()})`
         })
         .join('\n')
+
+      markdown += `\n\n${items}\n`
     } else {
-      if (markdown.lastIndexOf('\n') > 0) {
-        markdown += '\n\n'
-      } else markdown += '\n'
-      // Otherwise, format as category header and recursively generate list
-      markdown += isSubCategory ? `##### _${key.replaceAll("#", "\\#")}_\n` : `#### ${key.replaceAll("#", "\\#")}\n`
+      markdown += `\n\n${isSubCategory ? `##### _${key.replaceAll('#', '\\#')}_` : `#### ${key.replaceAll('#', '\\#')}`}\n`
       markdown += generateGitHubMarkdownList(
         obj[key] as GroupedData,
         `${pathPrefix}/${key}`,
-        true
+        true,
       )
     }
   })
@@ -98,4 +79,17 @@ export {
   groupByCategories,
   hasAnyArrayWithValues,
   installVsCodeExtension,
+}
+
+async function installVsCodeExtension(extensions: string[]) {
+  try {
+    for (const extension of extensions) {
+      const { stdout } = await execAsync(
+        `code --install-extension ${extension}`,
+      )
+      console.log(`Successfully installed ${extension}:`, stdout)
+    }
+  } catch (error) {
+    console.error('Error during installation:', error)
+  }
 }
--- scripts/mapContent.ts
+++ scripts/mapContent.ts
@@ -3,15 +3,19 @@ import { generateGitHubMarkdownList, groupByCategories } from './helpers.js'
 
 function mapContent() {
   const paths = getMdPaths('content', [])
-
   const separatedPaths = paths.map((path) => path.split('/').slice(1))
+
   const generatedMarkdownList = generateGitHubMarkdownList(
     groupByCategories(separatedPaths),
     'https://github.com/offshorly/offshorly-docs/tree/main/content',
   )
-  const markdown = `### Table of Contents
-${generatedMarkdownList}
-  `
+
+  const markdown = `
+### Table of Contents
+
+${generatedMarkdownList.trim()}
+
+`
 
   return markdown
 }
